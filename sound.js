30f1a46d3c420acd641e67288a228ad918cc2324
islascurieltechnologies <lalo@islascurieltechnologies.com>
2025-06-29 19:56:50 -0600

    Add files via upload
    Primera carga del sitio – Archivos base subidos desde el cel 😎

---
+++ sound.js
@@ -0,0 +1,6 @@
+
+function playIntroSound() {
+    const audio = new Audio('media/intro.mp3');
+    audio.volume = 0.2;
+    audio.play().catch(e => console.log("Audio no se pudo reproducir automáticamente."));
+}
